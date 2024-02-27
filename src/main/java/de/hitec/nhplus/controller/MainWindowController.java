f25d193921df84668616ea012a5a3e73b1bb819b
Tobias Thiel <t.thiel@schule.bremen.de>
2024-02-27 22:47:56 +0100

    refactored

---
+++ src/main/java/de/hitec/nhplus/controller/MainWindowController.java
@@ -0,0 +1,37 @@
+package de.hitec.nhplus.controller;
+
+import de.hitec.nhplus.Main;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+
+public class MainWindowController {
+
+    @FXML
+    private BorderPane mainBorderPane;
+
+    @FXML
+    private void handleShowAllPatient(ActionEvent e) {
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("/de/hitec/nhplus/AllPatientView.fxml"));
+        try {
+            mainBorderPane.setCenter(loader.load());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        AllPatientController controller = loader.getController();
+    }
+
+    @FXML
+    private void handleShowAllTreatments(ActionEvent e) {
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("/de/hitec/nhplus/AllTreatmentView.fxml"));
+        try {
+            mainBorderPane.setCenter(loader.load());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        AllTreatmentController controller = loader.getController();
+    }
+}
